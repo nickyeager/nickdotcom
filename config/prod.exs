12a5f18d17d3c7450bb171fc9be61607fe65e2cd
Nick Yeager <nick@quoteapro.com>
2020-08-12 18:49:15 -0700

    try changing the port call 'again'

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :nickdotcom, NickdotcomWeb.Endpoint,
        load_from_system_env: true,
          # Don't forget to replace pure-peak-67829 with the name of your Phoenix application that was created.
-       http: [port: {:system, "PORT"}],
+       http: [port: System.get_env("PORT")],
        url: [scheme: "https", host: "https://fast-depths-12980.herokuapp.com", port: 443],
        cache_static_manifest: "priv/static/cache_manifest.json",
        secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
